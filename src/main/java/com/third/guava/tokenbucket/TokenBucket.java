0803598a8448ca3ce7b2134cd5453f6d201efc97
squirrel <squirrelzt@126.com>
2019-04-26 15:16:43 +0800

    guava令牌桶api限流

---
+++ src/main/java/com/third/guava/tokenbucket/TokenBucket.java
@@ -0,0 +1,58 @@
+package com.third.guava.tokenbucket;
+
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
+import com.google.common.util.concurrent.RateLimiter;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.Executors;
+
+/**
+ * 类名称: TokenBucket
+ * 类描述: 令牌桶限流
+ * @author shiqianghui
+ * @date 2019-04-26
+ */
+public class TokenBucket {
+    public static void main(String[] args) {
+        ListeningExecutorService listeningExecutorService = MoreExecutors.listeningDecorator(Executors.newFixedThreadPool(100));
+
+        // 指定每秒放一个令牌
+        RateLimiter limiter = RateLimiter.create(1);
+        for (int i = 1; i < 50; i++) {
+            // 请求RateLimiter, 超过permits会被阻塞
+
+            //acquire(int permits)函数主要用于获取permits个令牌，并计算需要等待多长时间，进而挂起等待，并将该值返回
+            Double acquire = null;
+            if (i == 1) {
+                acquire = limiter.acquire(1);
+            } else if (i == 2) {
+                acquire = limiter.acquire(10);
+            } else if (i == 3) {
+                acquire = limiter.acquire(2);
+            } else if (i == 4) {
+                acquire = limiter.acquire(20);
+            } else {
+                acquire = limiter.acquire(2);
+            }
+            listeningExecutorService.submit(new Task("获取令牌成功, 获取耗 " + acquire + " 第 " + i + " 个任务执行"));
+
+
+        }
+    }
+}
+ class Task implements Runnable {
+
+    String str;
+
+    public Task (String str) {
+        this.str = str;
+    }
+
+    @Override
+     public void run() {
+         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+         System.out.println(sdf.format(new Date()) + " | " + Thread.currentThread().getName() + str);
+     }
+ }
