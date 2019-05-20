a921f12232f4f2eca322b56b9dbb989f3135002a
squirrel <squirrelzt@126.com>
2019-05-20 17:38:56 +0800

    currentTimeMillis

---
+++ src/main/java/com/useage/date/CurrentTimeUsage.java
@@ -0,0 +1,8 @@
+package com.useage.date;
+
+public class CurrentTimeUsage {
+    public static void main(String[] args) {
+        long time = System.currentTimeMillis();
+        System.out.println(time);
+    }
+}
