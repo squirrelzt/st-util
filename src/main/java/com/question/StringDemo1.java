a7d8a379ef9152927b0de09d734a5e9e682ec4f7
squirrel <squirrelzt@126.com>
2019-07-09 16:52:04 +0800

    String

--- src/main/java/com/question/StringDemo1.java
+++ src/main/java/com/question/StringDemo1.java
@@ -16,5 +16,12 @@ public class StringDemo1 {
         String e = d + 2;
         System.out.println((a == c));
         System.out.println((a == e));
+
+        System.out.println("-----------------------");
+        String str1 = new StringBuilder("计算机").append("软件").toString();
+        // String str3= new StringBuilder("计算机软件").toString();
+        System.out.println(str1.intern() == str1);
+        String str2 = new StringBuilder("Java(TM) SE ").append("Runtime Environment").toString();
+        System.out.println(str2.intern() == str2);
     }
 }
