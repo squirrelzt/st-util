74b8d5026358350a13b6ef8003c82a241e1af8d0
squirrelzt <squirrelzt@126.com>
2020-12-17 16:46:58 +0800

    Integer

---
+++ src/main/java/com/useage/IntegerDemo.java
@@ -0,0 +1,9 @@
+package com.useage;
+
+public class IntegerDemo {
+    public static void main(String[] args) {
+        Integer test = 0;
+        System.out.println((test==0));
+        System.out.println("0".equals(test));
+    }
+}
